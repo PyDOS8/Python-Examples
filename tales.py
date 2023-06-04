f7f8352f20b4fb1c0ae6f33cae49f112ad289981
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-04 18:51:13 -0400

    Create tales.py

---
+++ tales.py
@@ -0,0 +1,15 @@
+def game():
+  score = 0
+  if score == 10:
+    print("You win!")
+  import random
+  choices = ["heads", "tales"]
+  computer_choice = random.choice(choices) 
+  user_choice = input("Heads or tales > ")
+  if user_choice == computer_choice:
+    print("Correct!")
+    score = score+1#increase the score by 1
+    game()
+  else:
+    print("Try again!")
+  game()
