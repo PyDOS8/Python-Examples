777bc2d542c1212cb16594bfb90f1b98e04f3f4b
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-06-04 18:55:09 -0400

    Create random_number.py

---
+++ random_number.py
@@ -0,0 +1,5 @@
+#This program outputs a random number from 1 to 12.
+import random
+numbers = ["1", "2", "3", "4" , "5", "6", "7", "8", "9", "10", "11", "12"]
+random_choice = random.choice(numbers)
+print(random_choice)
